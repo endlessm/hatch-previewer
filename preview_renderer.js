189e4bb398afea7abe21d134df0abf36627de331
Srdjan Grubor <sgnn7@sgnn7.org>
2017-03-29 14:37:23 -0500

    Added ability to see source code in html articles and preview images
    With bootstrap here, we can now make this pretty and add a button that
    flips the content and source for html pages. With the ne code, we've
    also added in the image previewing functionality.

--- preview_renderer.js
+++ preview_renderer.js
@@ -4,20 +4,58 @@ $ = require('jquery')
 assetMap = main.assetMap
 hatchFolder = main.hatchFolder
 
+showingSource = false;
+
 $(document).ready(function(){
+  $('#flip_controls').hide();
+  $('#source_code').hide();
 })
 
+flipPage = function() {
+  if (showingSource) {
+    $('#preview').hide();
+    $('#source_code').show();
+  } else {
+    $('#source_code').hide();
+    $('#preview').show();
+  }
+  showingSource = !showingSource;
+}
+
 setAssetID = function(ID) {
-  if (ID != null) {
-    var asset = assetMap.get(ID)
-    console.log(asset)
-    $('#preview').html(asset.document)
+  if (!ID) {
+    $('#preview').html("<center><h2>No asset selected</h2></center>")
+    return;
+  }
+
+  showingSource = false;
+  $('#source_code').hide();
+  $('#preview').show();
+  $('#image_asset').remove();
+
+  var asset = assetMap.get(ID)
+
+  // Enable controls if we're an html document
+  if (asset.document) {
+    $('#flip_controls').show();
+    $('#source_code_content').text(asset.document).html();
+  } else {
+    $('#flip_controls').hide();
+  }
+
+  $('#preview').html(asset.document || "")
+  if (asset.document) {
     $('#preview img').each(function() {
       var imgID = $(this).attr("data-soma-job-id")
       $(this).attr("src", hatchFolder + "/" + imgID + ".data")
     })
+  } else if (asset.objectType == "ImageObject") {
+    image_asset = $('<img />', { id: 'image_asset',
+                                 src: asset.path,
+                                 class: 'm-3 m-x-auto d-block' });
+    image_asset.appendTo($('#preview'));
   } else {
-    $('#preview').html("<h2>No asset selected</h2>")
+    $('#preview').html("<center><h2>Unsupported asset type (" + asset.objectType + ")!</h2></center>")
   }
 }
 
